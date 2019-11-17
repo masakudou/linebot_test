d409ad22c630e9011833786e1d46df54f62b85f9
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-11-17 23:48:47 +0900

    Create app.go file.

---
+++ app.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/line/line-bot-sdk-go/linebot"
+    "github.com/line/line-bot-sdk-go/linebot/httphandler"
+)
+
+func main() {
+	// LINE bot SDKに含まれるhttpHandlerの初期化
+	handler, err := httphandler.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	// 初期化に失敗した場合
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ポート番号 環境変数がセットされていない場合は8080
+	port := os.Getenv("PORT")
+	if port = "" {
+		port = "8080"
+		log.Printf("ポート番号にデフォルト値 8080 をセットします.", port)
+	}
+
+	// リクエストを受け取った時に実行する関数を定義し、ハンドラに登録
+	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
+		bot, err := handler.NewClient()
+		// クライアント初期化に失敗
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		for _, event := range events {
+			// Event種別がメッセージでなければ終了
+			if event.Type != linebot.EventTypeMessage {
+				return
+			}
+
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				// replyText: message.Text(ユーザーが投稿したメッセージ)を代入する
+				replyText := message.Text
+				// replyTextの内容を送信
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyText)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	})
+
+	// /callback にエンドポイントの定義
+	http.Handle("/callback", handler)
+	// HTTPサーバの起動
+	log.Printf("ポート番号: %s をリッスン...", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
